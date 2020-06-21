124708fa3cdcdcffb31cea4e3217e7fb5af89e82
Dominador G de la Cruz Jr <domenikos858@gmail.com>
2020-06-21 01:12:59 -0700

    work still in progress

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+-- Select the burgers_db
+USE burgers_db;
+-- Insert new rows of data.
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("hamburger", false), ("cheeseburger", false), ("tofu burger", false);
